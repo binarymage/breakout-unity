31621cfba7fbd88e920c1a091d0a39551eb7bc21
George Shore <george@georgeshore.com>
2016-03-05 10:49:16 +0000

    Use static instance of MusicPlayer

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -3,16 +3,15 @@ using System.Collections;
 
 public class MusicPlayer : MonoBehaviour
 {
+	static MusicPlayer instance = null;
 
-	// Use this for initialization
-	void Start ()
+	void Awake()
 	{
-		GameObject.DontDestroyOnLoad(gameObject);
-	}
-	
-	// Update is called once per frame
-	void Update ()
-	{
-	
+		if (instance != null) {
+			Destroy(gameObject);
+		} else {
+			instance = this;
+			GameObject.DontDestroyOnLoad(gameObject);
+		}
 	}
 }
