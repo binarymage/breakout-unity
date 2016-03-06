d9097b721f2ce5d0e9940af753f7de3f51c2de69
George Shore <george@georgeshore.com>
2016-03-06 00:04:20 +0000

    Add colliders

---
+++ Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoseCollider : MonoBehaviour
+{
+	void OnTriggerEnter2D(Collider2D collider)
+	{
+		Debug.Log("Trigger!");
+	}
+
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		Debug.Log("Collision!");
+	}
+}
