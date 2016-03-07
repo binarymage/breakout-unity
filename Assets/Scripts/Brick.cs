4e37483cf00a9b35e974320deae2068a8a1cfa81
George Shore <george@georgeshore.com>
2016-03-07 22:33:34 +0000

    Add brick behaviour

---
+++ Assets/Scripts/Brick.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Brick : MonoBehaviour {
+
+    public int maxHits;
+    private int timesHit;
+
+	// Use this for initialization
+	void Start () {
+        timesHit = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (timesHit == maxHits)
+        {
+            Destroy(gameObject);
+        }
+	}
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        timesHit++;
+    }
+}
