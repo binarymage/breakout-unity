e9d0fda28494bc8ee7e3cf775a1b3b1754d63dfe
George Shore <george@georgeshore.com>
2016-03-07 08:41:15 +0000

    Move paddle with mouse

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+	void Update () {
+        float x = Mathf.Clamp(Input.mousePosition.x / Screen.width * 16, 0.75f, 15.25f);
+        transform.position = new Vector3(x, transform.position.y, transform.position.z);
+	}
+}
