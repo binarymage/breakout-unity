52ac38f989490b1aa2a09eda874335899584aeee
George Shore <george@georgeshore.com>
2016-03-08 21:12:51 +0000

    Add Lose scene

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -3,10 +3,17 @@ using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
+    private int level;
 
     public void LoadLevel(string name)
     {
         SceneManager.LoadScene(name);
+    }
+
+    public void LoadNextLevel()
+    {
+        level++;
+        SceneManager.LoadScene(level);
     }
 
     public void QuitGame()
