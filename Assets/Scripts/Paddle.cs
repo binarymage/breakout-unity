cf05309a02cdc55f89efa203485e9d8969268b8a
George Shore <george@georgeshore.com>
2016-03-22 12:00:38 +0000

    Add automated movement for testing

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -2,9 +2,23 @@
 
 public class Paddle : MonoBehaviour
 {
+    public bool autoPlay;
+
+    private Ball ball;
+
+    void Start()
+    {
+        ball = FindObjectOfType<Ball>();
+    }
+
     void Update()
     {
-        float x = Mathf.Clamp(Input.mousePosition.x / Screen.width * 16, 0.75f, 15.25f);
+        float x = Mathf.Clamp(getXMovement(), 0.75f, 15.25f);
         transform.position = new Vector3(x, transform.position.y, transform.position.z);
+    }
+
+    private float getXMovement()
+    {
+        return autoPlay ? ball.transform.position.x : Input.mousePosition.x / Screen.width * 16;
     }
 }
