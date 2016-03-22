1460c2ac7173c84e948ceec4bcf4193fdd4ac042
George Shore <george@georgeshore.com>
2016-03-22 12:02:50 +0000

    Format document

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -3,19 +3,23 @@ using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
-	public void BrickDestroyed() {
-		if (Brick.breakableCount <= 0) {
-			LoadNextLevel();
-		}
-	}
+    public void BrickDestroyed()
+    {
+        if (--Brick.breakableCount <= 0)
+        {
+            LoadNextLevel();
+        }
+    }
 
     public void LoadLevel(string name)
     {
+        Brick.breakableCount = 0;
         SceneManager.LoadScene(name);
     }
 
     public void LoadNextLevel()
     {
+        Brick.breakableCount = 0;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
